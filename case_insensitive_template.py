576ba130644799f0a2ad2d9cc275ab53347aebdf
Kranthos <64779323+Kranthos@users.noreply.github.com>
2020-05-04 12:57:32 +0200

    Add files via upload

---
+++ case_insensitive_template.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+
+command_list = ["#INPUT_TEXT ", "#INPUT_TEXT ", "#INPUT_TEXT "] 
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or(*command_list), case_insensitive=True)
+
+@bot.event
+
+async def on_ready():
+    print('#INPUT_TEXT ')
+
+@bot.command()
+
+async def """#REPLACE_BY_TEXT""" (ctx):
+
+    await ctx.send("#INPUT_TEXT ")
+
+bot.run("#REPLACE_BY_TOKEN e.i sddsf7jgsf6d5g-dhdg2hf4g4hf5gh-Yw#") #REPLACE BY YOUR TOKEN
+
+
+
+
+
+
