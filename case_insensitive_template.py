7d518ca4169e9b5159b19eee2a84af58cc2a58c4
Kranthos <64779323+Kranthos@users.noreply.github.com>
2021-12-06 22:47:38 +0100

    Update case_insensitive_template.py

--- case_insensitive_template.py
+++ case_insensitive_template.py
@@ -7,10 +7,10 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or(*command_list), cas
 
 @bot.event
 async def on_ready():
-    print('#INPUT_TEXT ')
+    print('bot is online!')
 
 @bot.command()
-async def """#REPLACE_BY_TEXT""" (ctx):
-    await ctx.send("#INPUT_TEXT")
+async def hello(ctx):
+    await ctx.send("hello")
 
 bot.run("#REPLACE_BY_TOKEN e.i sddsf7jgsf6d5g-dhdg2hf4g4hf5gh-Yw#") #REPLACE WITH YOUR TOKEN
